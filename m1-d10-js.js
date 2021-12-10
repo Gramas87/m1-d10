3ba417961da81954a77b280ade911aaeb9a81967
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2021-12-10 16:07:39 +0100

    homework

---
+++ m1-d10-js.js
@@ -0,0 +1,65 @@
+let x = "John"
+let y = "Doe"
+console.log(x + " <> " + y)
+
+const obj = {
+    name: "",
+    surname:  "",
+    email: ""
+    }
+
+delete obj.email
+
+array = ["", "", "", "", "", "", "", "", "", "", "" ]
+
+console.log(array)
+
+let array100=[]
+/*
+for(i=0; i<100; i++){
+    array100[i]=Math.floor(Math.random()*100)+1
+}
+
+const getMinAndMaxValues = function(){
+let getArrayMax= Math.max.apply(null, array100)   
+let getArrayMin=math.max.apply(null, array100)
+
+getArraymax
+
+}
+     console.log(getMinAndMaxValues) bhooooooooooo... */
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//                     dom  
+
+
+
+let container=document.getElementById("container")
+
+let td = document.getElementsByTagName("td")
+
+for (let i = 0; i < td.length; index++) {
+    const element = td[i];
+     element.innerText("prova")
+    
+}
