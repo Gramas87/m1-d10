c8a59af710deb4564415e5e2e80dc0bb2e8f24ac
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2021-12-12 10:22:36 +0100

    exercies done

--- m1-d10-js.js
+++ m1-d10-js.js
@@ -10,47 +10,170 @@ const obj = {
 
 delete obj.email
 
-array = ["", "", "", "", "", "", "", "", "", "", "" ]
+array = []
+for (let i = 0; i <= 10; i++) {
+    array[i]=("im array number" + i)
 
-console.log(array)
+    
+}
+//console.log(array.join(", "))
 
-let array100=[]
-/*
+for (let i = 0; i < array.length; i++) {
+     array
+console.log(array[i])    
+}
+
+
+
+
+array2 = []
 for(i=0; i<100; i++){
-    array100[i]=Math.floor(Math.random()*100)+1
+    array2[i]=Math.floor(Math.random()*100)+1
 }
-
+console.log(array2)
 const getMinAndMaxValues = function(){
-let getArrayMax= Math.max.apply(null, array100)   
-let getArrayMin=math.max.apply(null, array100)
-
-getArraymax
-
+console.log(Math.max(...array2));
+console.log(Math.min(...array2))
+    
 }
-     console.log(getMinAndMaxValues) bhooooooooooo... */
+     
+
+getMinAndMaxValues()
+
+function getMaxMin(numbers) {
+    let max = numbers[0]
+    let min = numbers[0]
+
+   for (let number of numbers){
+       max = number > max ? numbers : max
+       min = number < min ? numbers : min
+   }
+
+   return {max, min}
+ }
+/*
+let arrcontainer = []
+for (let i = 0; i<10; i++) {
+    randomnumber=Math.floor(Math.random()*90)+1
+    */
+    
+
+
+ 
+
+    
 
 
 //                     dom  
 
 
 
-let container=document.getElementById("container")
+
+document.getElementById("container").style.color = "blue"
+
+
+
+
 
 let td = document.getElementsByTagName("td")
 
 
 
-for (let i = 0; i < td.length; index++) {
-    let element = td[i];
-     element.innerText="Hello"
+for (let i = 0; i < td.length; i++) {
+
+    console.log(td[i].textContent)
     
 }
 
 
 
-let head = document.getElementsByTagName("h1")
 
-function ChangeHead(text){
-    head.innerText(text)
+function changeHead(){
+    document.getElementsByTagName("h1")[0].innerHTML= "Hello";
+ }
+
+changeHead()
+
+function extraRow() {
+    document.querySelector("table")
+    let table = document.querySelector("table")
+
+
+ let newRow = table.insertRow()
+ for (let i = 0; i < 5; i++) {
+    newRow.insertCell(0)
+     
+     
+ }
+ 
 }
-console.log(ChangeHead="hello")
+extraRow()
+/*
+function write(text){
+    let table= document.querySelector("table")
+    
+    let row = table.lastChild
+
+    let row2 = row.lastChild
+ 
+  
+      console.log(row2)
+
+  }
+  write("aa")
+  */
+  
+    
+   
+    
+
+
+
+
+function addClass(){
+    const row = document.querySelectorAll("tr")
+   for (let i = 0; i < row.length; i++) {
+       
+    row[i].classList.add("test");
+   }
+    
+
+    
+    
+    console.log(row)
+
+}
+addClass()
+
+function changecolor(){
+  let link = document.querySelectorAll("a")
+  for (let i = 0; i < link.length; i++) {
+      link[i].style.backgroundColor = 'red'
+      
+  }
+}
+changecolor()
+
+window.onload = function () { 
+    console.log("Page Loaded")
+ }
+
+ let addItems = document.querySelector("ul")
+ function add1(){
+    let newli = document.createElement("li")
+    newli.innerText="ciao"
+addItems.appendChild(newli)
+}
+ add1()
+ 
+ console.log(addItems)
+
+ function empty(){
+
+     const list = document.querySelectorAll("ul > li")
+     for (let i = 0; i < list.length; i++) {
+         list[i].innerText = ""
+         
+     }
+     }
+ empty()
